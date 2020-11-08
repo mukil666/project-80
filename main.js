1eba67cd7d7e2aa1550b341334638ab86aec0e89
mukil666 <72088762+mukil666@users.noreply.github.com>
2020-11-08 21:40:01 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function getParagraph1(){
+    var inputs = [];
+    for(i =1 ; i <= 4 ; i++)
+    {
+        inputs.push(document.getElementById("para1_input_box_" + i).value);
+    }
+    inputs.join(".");
+    document.getElementById("showParagraph1").innerHTML = inputs.join(".")
+}
+
+function getParagraph2(){
+    var inputs = [];
+    for(i =1 ; i <= 4 ; i++)
+    {
+        inputs.push(document.getElementById("para2_input_box_" + i).value);
+    }
+    inputs.join(".");
+    document.getElementById("showParagraph2").innerHTML = inputs.join(".")
+}
